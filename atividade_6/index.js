a27f92ae896300eed3db2aa520b1567ea75ee4b3
Josiane Rocha <66445562+josiane-rocha@users.noreply.github.com>
2020-07-15 21:30:04 -0300

    Atividade 6
    Exercícios

--- atividade_6/index.js
+++ atividade_6/index.js
@@ -1,5 +1,24 @@
 // ## Importe a biblioteca ##
 
-
+/*Perguntar ao usuário o seu nome
+Pedir para o usuário digitar qualquer coisa
+Contar quantos caracteres tem no que o usuário digitou
+Informar o número de caracteres ao usuário (Ex.: "Você digitou 32 caracteres!")
+Mostrar para o usuário qual foi a pessoa que digitou mais caracteres*/
 
 // ## Faça o código ##
+
+console.log("Bem vindo!")
+
+let rs = require("readline-sync")
+
+let nome = rs.question("Qual o seu nome? ")
+let palavra = rs.question("Digite uma palavra: ")
+
+let palavra = 0
+let palavra = 1000
+
+for(let i = 0; i <=1000,; i++) {
+    console.log ("Você digitou "" caracteres")
+    contador++
+}
