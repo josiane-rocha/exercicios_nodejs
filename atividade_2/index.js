2d8b221e6a6c2d6d10b753100543b0ea4fc907fd
Josiane Rocha <66445562+josiane-rocha@users.noreply.github.com>
2020-07-15 21:25:45 -0300

    Atividade 2
    Exercícios

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,23 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require('readline-sync')
 
 // ## Faça o código ##
+
+let = rs.question (console.log("Você está usando máscara quando precisa sair? ('S ou N') \n"))
+
+if (mask = 'S && N') {
+    console.log("Parabéns, continue assim, essa medida é ótima para a segurança de todos!!!");
+} else {
+    console.log("Essa atitude está errada, por favor coloque sua máscara para ajudar na segurança de todos!!!");
+}
+
+console.log ("================================================================================")   
+
+let = rs.question (console.log("Você está usando lavando as mãos frequentemente? (S ou N) \n"));
+
+if (mao = 'S => N') {
+    console.log("Parabéns, continue assim, essa medida é ótima para a segurança de todos!!!");
+} else {
+    console.log("Essa atitude está errada, por favor coloque sua máscara para ajudar na segurança de todos!!!");
+}
