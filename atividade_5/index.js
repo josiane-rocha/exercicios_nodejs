7d72ac848d82f6ae0f5b7dac98dd967c396ae839
Josiane Rocha <66445562+josiane-rocha@users.noreply.github.com>
2020-07-15 21:28:12 -0300

    Atividade 5
    Exercícios

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,17 @@
 // ## Importe a biblioteca ##
 
-
+/*Pedir para o usuário digitar qualquer coisa
+Contar quantos caracteres tem no que o usuário digitou
+Informar o número de caracteres ao usuário (Ex.: "Você digitou 32 caracteres!")*/
 
 // ## Faça o código ##
+
+let rs = require('readline-sync')
+
+let palavra = rs.question(console.log("Escreva uma palavra"))
+
+let contador = 0
+
+if (contador * palavra) {
+    console.log(contador * palavra)
+}
