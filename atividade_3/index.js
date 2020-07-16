7fd45f1b864835ee38f0f636251dd24065ae1bef
Josiane Rocha <66445562+josiane-rocha@users.noreply.github.com>
2020-07-15 21:26:31 -0300

    Atividade 3
    Exercícios

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,13 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require('readline-sync')
 
 // ## Faça o código ##
+
+let laterais = rs.question (console.log("Qual a medida das laterais do quadrado?"))
+let contador = 4
+
+while (contador * laterais) {
+    console.log(contador * laterais)
+    break
+}
